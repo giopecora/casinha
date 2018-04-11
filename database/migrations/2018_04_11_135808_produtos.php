2c2274ce0ff09f63590bacdfa9da68151e79e76e
Giovane Pecora <giovanepecora@gmail.com>
2018-04-11 11:20:34 -0300

    Primeiro Commit - Criacao do Projeto

---
+++ database/migrations/2018_04_11_135808_produtos.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Produtos extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (!Schema::hasTable('produtos')) {
+            Schema::create('produtos', function(Blueprint $table){
+                $table->string('codigoProduto',50);
+                $table->string('descricao',200);
+                $table->string('grupo',50);
+                $table->mediumInteger('qtdeDisponivel')->unsigned();
+                $table->mediumInteger('qtdeAlugada')->unsigned();
+                $table->string('nomeImagem',200);
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produtos');
+    }
+}
